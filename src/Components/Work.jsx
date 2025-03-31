8484eaf8f6193ec7ae9bdca86e621d3746b381b2
ParasP41 <parasvishwakarma198@gmail.com>
2025-04-01 00:07:44 +0530

    nv

--- src/Components/Work.jsx
+++ src/Components/Work.jsx
@@ -10,13 +10,12 @@ export default function Work({ ref }) {
         { name: "Climatic - Weather App", img: weather, link: "https://climatic-ivory.vercel.app/" },
         { name: "Zentry Clone", img: game, link: "https://zentry-five-henna.vercel.app/" },
         { name: "Tic-Tac-Toe Game", img: tic, link: "https://tic-tak-toe-sable.vercel.app/" },
-        { name: "Monkey App - News", img: news, link: "/" },
+        { name: "Monkey App - News", img: news, link: "https://news-monkey-zeta-one.vercel.app/" },
         { name: "iNoteBook", img: note, link: "https://i-note-book-omega.vercel.app/" },
     ];
 
     return (
         <div ref={ref} className="p-5">
-            {/* Heading */}
             <div className="pb-8 md:mx-3">
                 <p className="md:text-6xl md:mx-6 text-3xl mb-3 font-bold underline decoration-orange-500 underline-offset-10 primary-color">
                     Work
@@ -24,7 +23,6 @@ export default function Work({ ref }) {
                 <p className="text-2xl md:mx-5 text-white mb-3 font-bold">Check out some of my recent work</p>
             </div>
 
-            {/* Project Grid */}
             <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
                 {projects.map((project, index) => (
                     <div
@@ -33,10 +31,8 @@ export default function Work({ ref }) {
                             index === 0 || index === 3 ? "md:col-span-2" : ""
                         }`}
                     >
-                        {/* Project Image */}
                         <img src={project.img} alt={project.name} className="object-cover w-full h-full" />
 
-                        {/* Hover Overlay */}
                         <div className="opacity-0 group-hover:opacity-90 bg-[gray]/70 absolute inset-0 flex flex-col justify-center items-center">
                             <span className="text-2xl font-bold text-white tracking-wider">{project.name}</span>
                             <div className="pt-8 text-center">
